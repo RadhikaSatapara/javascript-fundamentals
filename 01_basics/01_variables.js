620b1c899a76dfae39880c1083fc2c4d9ace2e87
Radhika Satapara <148217462+RadhikaSatapara@users.noreply.github.com>
2026-03-12 17:15:29 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123
+let accountEmail = "radhika@gmail.com"
+var accountPass = "!2345"
+accountCity = "Jaipur"
+
+// accountId = 2
+console.log(accountId);
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountEmail,accountId,accountPass,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as newer version
+
+// alert("hello") // we are using nodeJs, not browser
+
+let name = "Radhika"
+let age = 23
+let isLoggedIn = false
+let state =null
+let city;
+
+//number => 2 to power 53
+//bigInt 
+//string => ""
+//boolean => true or false
+//null => standalone value , intentionally empty
+//undefined => declared but not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof null); // object bcz it is an object
+console.log(typeof undefined); // undefined because it is type itself
+
