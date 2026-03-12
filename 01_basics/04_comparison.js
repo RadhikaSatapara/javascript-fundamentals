b5571cc3fdb5f42618cc5957a1656ff43daac91f
Radhika Satapara <148217462+RadhikaSatapara@users.noreply.github.com>
2026-03-12 18:44:22 +0000

    conversion and comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false 
+// console.log(null >= 0); //true
+
+// the reason is that an equality check == and comparisons > <>=
+// </ work differently.
+// Comparisons convert null to a number,treating it as0.
+// That's why (3) null>= 0 is true and (1) null >0 is false.
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+// ===
+
+console.log("2" === 2);
---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,34 @@
+// Primitive (call by values)
+
+// 7 types : String , Number , Boolean , null , undefined , symbol , BigInt
+
+const score = 100
+const scoreVslue = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+// const bigNumber = 344654654675n
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions 
+
+const heros = ["shaktiman", "nagraj", "doga"];
+
+let myObj = {
+    name: "radhika",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("Hello");
+}
+
+console.log(typeof outsideTemp)
